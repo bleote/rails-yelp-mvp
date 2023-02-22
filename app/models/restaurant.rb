baed642f8ebbd5ced48701ad29e6a82c1506e8c0
Bruno Leote <b.leote@gmail.com>
2023-02-22 17:10:37 +0100

    Yelp MVP

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,10 @@
 class Restaurant < ApplicationRecord
+  CATEGORY = ['Chinese', 'Italian', 'Japanese', 'French', 'Belgian']
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: CATEGORY }
+  validates :category, presence: true
+
+  has_many :reviews, dependent: :destroy
 end
